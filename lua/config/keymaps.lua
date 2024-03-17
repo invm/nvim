b6d8563cfcd5e3611da9c449a4c95ba7565e88d5
Michael Ionov <michael@appdome.com>
2024-03-17 18:45:22 +0200

    chore: update mappigns

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,61 @@
 local keys = {
   n = {
     { "<leader>js", "<cmd> :set filetype=json<CR>", { desc = "Set filetype to json" } },
+    { "<leader>ff", "<cmd> Telescope find_files <CR>", { desc = "Find files" } },
+    { "<leader>fw", "<cmd> Telescope live_grep <CR>", { desc = "Live grep" } },
+    { "<leader>fb", "<cmd> Telescope buffers <CR>", { desc = "Find buffers" } },
+    {
+      "]h",
+      function()
+        if vim.wo.diff then
+          return "]c"
+        end
+        vim.schedule(function()
+          require("gitsigns").next_hunk()
+        end)
+        return "<Ignore>"
+      end,
+      { desc = "Jump to next hunk" },
+    },
+
+    {
+      "[h",
+      function()
+        if vim.wo.diff then
+          return "[c"
+        end
+        vim.schedule(function()
+          require("gitsigns").prev_hunk()
+        end)
+        return "<Ignore>"
+      end,
+      { desc = "Jump to prev hunk" },
+    },
+
+    -- Actions
+    {
+      "<leader>rh",
+      function()
+        require("gitsigns").reset_hunk()
+      end,
+      { desc = "Reset hunk" },
+    },
+
+    {
+      "<leader>ph",
+      function()
+        require("gitsigns").preview_hunk()
+      end,
+      { desc = "Preview hunk" },
+    },
+
+    {
+      "<leader>gb",
+      function()
+        package.loaded.gitsigns.blame_line()
+      end,
+      { desc = "Blame line" },
+    },
     {
       "<leader>cx",
       function()
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,12 +5,25 @@ return {
       require("telescope").setup({
         defaults = {
           prompt_prefix = "   ",
+          layout_config = {
+            horizontal = {
+              prompt_position = "bottom",
+              preview_width = 0.65,
+              results_width = 0.8,
+            },
+            vertical = {
+              mirror = false,
+            },
+            width = 0.92,
+            height = 0.90,
+            preview_cutoff = 150,
+          },
           mappings = {
             i = {
-              ["<C-l>"] = "cycle_history_next",
-              ["<C-h>"] = "cycle_history_prev",
-              ["<C-j>"] = "move_selection_next",
-              ["<C-k>"] = "move_selection_previous",
+              ["<C-j>"] = "cycle_history_next",
+              ["<C-k>"] = "cycle_history_prev",
+              ["<C-h>"] = "preview_scrolling_left",
+              ["<C-l>"] = "preview_scrolling_right",
             },
           },
         },
