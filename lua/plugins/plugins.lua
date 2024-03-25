b5837d67c70ee9f9ddf1f7cb0c013d749f9f8946
Michael Ionov <imichaelionov@gmail.com>
2024-03-25 09:52:15 +0200

    chore: update suprttab

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -81,7 +81,15 @@ return {
     "hrsh7th/nvim-cmp",
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local luasnip = require("luasnip")
       local cmp = require("cmp")
+
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
@@ -92,10 +100,14 @@ return {
             else
               cmp.confirm()
             end
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
           else
             fallback()
           end
-        end, { "i", "s", "c" }),
+        end, { "i" }),
       })
     end,
   },
