6d3c3c9c2439b00017d5aec2ed41ae34248ab8df
Michael Ionov <imichaelionov@gmail.com>
2024-04-06 15:47:37 +0300

    chore: update neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,20 @@ return {
           ["<space>"] = "open",
         },
       },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".git",
+            ".DS_Store",
+            "node_modules",
+          },
+          never_show = {},
+        },
+      },
     },
   },
 }
