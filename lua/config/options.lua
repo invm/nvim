12f2f442a1faf4f329c76fb8d4e5e1656576b639
Michael Ionov <michael@appdome.com>
2024-04-10 11:42:17 +0300

    fix: normal cursor

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,6 +12,7 @@ local options = {
   updatetime = 0,
   foldexpr = "v:lua.vim.treesitter.foldexpr()",
   foldmethod = "manual",
+  guicursor = "n-v-c-sm:block,i-ci:ver30-iCursor-blinkwait200-blinkon150-blinkoff150,r-cr-o:hor20",
 }
 
 vim.g.autoformat = false
