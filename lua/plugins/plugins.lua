e8179eb3020a80a3f2b866048772de6fdf2286cf
Michael Ionov <imichaelionov@gmail.com>
2024-04-12 22:29:18 +0300

    chore: add tailwind disable notify

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "folke/noice.nvim",
+    opts = {
+      notify = {
+        enabled = false,
+      },
+    },
+  },
+  { "rcarriga/nvim-notify", enabled = false },
+  {
     "kdheepak/lazygit.nvim",
     cmd = {
       "LazyGit",
