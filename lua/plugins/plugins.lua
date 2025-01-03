0ea351f44331f5f4b25d8875c329405dd1cfcde0
Michael Ionov <imichaelionov@gmail.com>
2025-01-03 17:14:24 +0200

    feat: add anthropic to code companion

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -4,14 +4,20 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
     config = true,
     ops = {
+      strategies = { chat = { adapter = "anthropic" }, inline = { adapter = "anthropic" } },
       adapters = {
         openai = function()
           return require("codecompanion.adapters").extend("openai", {
-            env = {
-              api_key = "",
-            },
+            env = { api_key = "" },
           })
         end,
+        adapters = {
+          anthropic = function()
+            return require("codecompanion.adapters").extend("anthropic", {
+              env = { api_key = "" },
+            })
+          end,
+        },
       },
     },
   },
